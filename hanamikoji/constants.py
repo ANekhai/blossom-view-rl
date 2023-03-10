2fd58a67d981253b639ffb6d8faf84ec1fb3117e
Anton <nekhai.anton@gmail.com>
2023-03-09 23:29:23 -0500

    Added constant value dicts

---
+++ hanamikoji/constants.py
@@ -0,0 +1,34 @@
+# Define some constants for use later
+GEISHA_COLORS = {
+    1: "Purple",
+    2: "Red",
+    3: "Yellow",
+    4: "Blue",
+    5: "Orange",
+    6: "Green",
+    7: "Pink"
+}
+
+GEISHA_POINTS = {
+    1: 2,
+    2: 2,
+    3: 2,
+    4: 3,
+    5: 3,
+    6: 4,
+    7: 5,
+}
+
+ACTION_NAMES = {
+    1: "Secret",
+    2: "Trade-off",
+    3: "Gift",
+    4: "Competition",
+}
+
+ACTION_DESCRIPTIONS = {
+    1: "Place a card face down. This card will be scored at the end of the round.",
+    2: "Place two cards face down. These cards will not be scored.",
+    3: "Pick three cards. Your opponent chooses one and gets one card, you get the other two.",
+    4: "Pick 2 piles of 2 cards. Your opponent chooses one pile and gets it, you get the other."
+}
